bc0d5ad015d00b83869d7f2dbded1f99767ac33d
luannmoraess <104633782+luannmoraess@users.noreply.github.com>
2022-05-05 09:56:42 -0300

    new
    new

---
+++ PROJETO-INTEGRADOR-main/PROJETO-INTEGRADOR-main/js/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+
+    window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+    
+    })
+
+}
